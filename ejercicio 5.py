7404fdce1db40230b5095237af0a251cba7f99c7
Sofia-estrada <s.estrada@udea.edu.co>
2025-03-30 19:29:05 -0500

    Update ejercicio 5.py

--- ejercicio 5.py
+++ ejercicio 5.py
@@ -1,14 +1,20 @@
-factorial=1
-c=1
 while True:
-    f=int(input("Ingrese un número entre 1 y 20: "))
-    if f>=1 and f<=20:
+    factorial=1
+    c=1
+    while True:
+        f=int(input("Ingrese un número entre 1 y 20: "))
+        if f>=1 and f<=20:
+            break
+        else:
+            print("Valor no aceptado")
+    while c<=f:
+        factorial *=c
+        c+=1
+    print(f"El factorial de {f}! es:", factorial)
+    p=int(input("Desea continuar? 1-SI, 2-NO: "))
+    if p!=1:
+        print ("Ok, hasta la próxima")
         break
-    else:
-        print("Valor no aceptado")
-while c<=f:
-    factorial *=c
-    c+=1
-print(f"El factorial de {f}! es:", factorial)
+        
         
 
